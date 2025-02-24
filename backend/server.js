1cbb8119baa70b7ff93ad80136763700a4b11cdf
regis066 <mugisharegis72@gmail.com>
2025-02-24 14:21:35 +0200

    use authRoutes middleware and connectToMongoDB()

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,18 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 
 const app = express();
+dotenv.config();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server listening on http://localhost:${PORT}`)
+});
