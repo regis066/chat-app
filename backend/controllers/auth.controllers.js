9678636647ba6b4d76310749bbb10ea3fb0c3c41
regis066 <mugisharegis72@gmail.com>
2025-02-24 15:02:34 +0200

    implement user sign up business logic

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,49 @@
-export const signup = (req, res) => {
-  console.log("signup");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        error: "Password do not match",
+      });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({
+        error: "Username already exists",
+      });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender == "male" ? boyProfilePic : girlProfilePic
+    });
+    await newUser.save();
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    console.log("Error in signing up", error.message);
+    ;
+    res.status(500).json({ error: "Internal Server error" });
+  }
 };
 
 export const login = (req, res) => {
