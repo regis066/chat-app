db909717f819b01234b5742afbc96e14228d0bfa
regis066 <mugisharegis72@gmail.com>
2025-02-25 21:48:43 +0200

    update

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = async (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV === "production",
   });
 };
 
