29ec819b677a394d6066cc2696398ca2d80a0ab1
regis066 <mugisharegis72@gmail.com>
2025-02-25 21:46:56 +0200

    create Conversation model

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+
+const conversationSchema = Schema({
+    participants: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    messages: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Message',
+            default: []
+        }
+    ]
+}, {
+    timestamps: true
+})
+
+const Conversation = mongoose.model('Conversation', conversationSchema);
+
+export default Conversation;
