2ed99ce3ab71aff8d41acb5c24ff68869b81e4d5
regis066 <mugisharegis72@gmail.com>
2025-02-25 21:45:24 +0200

    add send message functionality

---
+++ backend/controllers/message.controllers.js
@@ -0,0 +1,38 @@
+import { request } from "express";
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id : receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] }
+    });
+    if (!conversation) {
+      conversation = new Conversation({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage =new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+    await newMessage.save();
+    await conversation.save();
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
