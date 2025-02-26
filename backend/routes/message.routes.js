2dab27cd8afd2d84329e1b4b94a13093c79c2ad4
regis066 <mugisharegis72@gmail.com>
2025-02-26 21:33:30 +0200

    create route to get messages between sender and receiver

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controllers.js";
+import { getMessages, sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 
 const router = express.Router();
 
 router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
