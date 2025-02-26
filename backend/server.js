055b7b5472f2cdfdbbd9fbab54577e1400a8728c
regis066 <mugisharegis72@gmail.com>
2025-02-26 21:31:22 +0200

    refactor

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 
-
 const app = express();
 dotenv.config();
-
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,8 +15,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+connectToMongoDB();
 
 app.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Server listening on http://localhost:${PORT}`)
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
