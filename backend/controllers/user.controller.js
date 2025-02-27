fc270d6114d79fa4e204af660cc0bd86bd3c8fac
regis066 <mugisharegis72@gmail.com>
2025-02-27 15:16:36 +0200

    add controller to get users for the sidebar

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,13 +1,12 @@
 import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
-    try {
-      
-        const loggedInUserId = req.user._id;
+  try {
+    const loggedInUserId = req.user._id;
 
-        const allUsers = await User.find({ _id: { $ne: loggedInUserId } });
+    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select('-password');
 
-
+    res.status(200).json(filteredUsers);
   } catch (error) {
     console.log("Error in getUsersForSidebar controller: ", error.message);
     res.status(500).json({ error: "Internal Server error" });
