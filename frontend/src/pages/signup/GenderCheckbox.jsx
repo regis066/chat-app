96e3c1929500cd22d653125736c295b5c71dc08a
regis066 <mugisharegis72@gmail.com>
2025-03-03 21:52:48 +0200

    feat: track the gender checkbox selected and add some tailwindcss styles

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,18 +1,37 @@
-import React from "react";
-
-const GenderCheckbox = () => {
+import PropTypes from "prop-types";
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
   return (
     <div className="flex gap-3 mt-2">
       <div className="form-control">
-        <label className="label">
-          <span className="label-text">Male </span>
-          <input type="checkbox" className="checkbox" />
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "male" ? "selected" : ""
+          }`}
+        >
+          <span className="label-text">Male</span>
+          <input
+            type="checkbox"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
+            name="male"
+            className="checkbox"
+          />
         </label>
       </div>
       <div className="form-control">
-        <label className="label">
-          <span className="label-text">Female </span>
-          <input type="checkbox" className="checkbox" />
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "female" ? "selected" : ""
+          }`}
+        >
+          <span className="label-text">Female</span>
+          <input
+            type="checkbox"
+            checked={selectedGender === "female"}
+            name="female"
+            className="checkbox"
+            onChange={() => onCheckboxChange("female")}
+          />
         </label>
       </div>
     </div>
