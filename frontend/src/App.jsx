1f7c6ef033157aa1d5832ff7f9ecf503c63a6b9e
regis066 <mugisharegis72@gmail.com>
2025-03-04 15:49:45 +0200

    feat: add Toaster and redirect user if authenticated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import { Route, Routes } from "react-router"
-import Home from "./pages/home/Home"
-import Login from "./pages/login/Login"
-import Signup from "./pages/signup/Signup"
-
+import { Navigate, Route, Routes } from "react-router";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 const App = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={authUser ? <Navigate to="/" /> : <Signup />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
