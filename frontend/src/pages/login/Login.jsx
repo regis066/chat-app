41b00adfe93f63a41f4814e5bf4c4d03617791bc
regis066 <mugisharegis72@gmail.com>
2025-03-04 16:53:24 +0200

    feat: add loading  spinner

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { loading, login } = useLogin();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    login(inputs);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full bg-transparent p-6 rounded-lg shadow-md bg-clip-padding backdrop-blur-lg">
@@ -10,14 +28,16 @@ const Login = () => {
           <span className="text-blue-500"> ChatApp</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleOnSubmit}>
           <div>
             <label className="label p-2">
               <span className="label-text text-base">Username</span>
             </label>
             <input
               type="text"
+              name="username"
               placeholder="Enter username"
+              onChange={handleInputChange}
               className="input input-bordered w-full h-10"
             />
           </div>
@@ -27,7 +47,9 @@ const Login = () => {
             </label>
             <input
               type="password"
+              name="password"
               placeholder="Enter password"
+              onChange={handleInputChange}
               className="input input-bordered w-full h-10"
             />
           </div>
@@ -39,7 +61,17 @@ const Login = () => {
           </Link>
 
           <div>
-            <button className="btn btn-sm btn-block mt-2">Login</button>
+            <button
+              type="submit"
+              className="btn btn-sm btn-block mt-2"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner loading-lg"></span>
+              ) : (
+                "Login"
+              )}
+            </button>
           </div>
         </form>
       </div>
