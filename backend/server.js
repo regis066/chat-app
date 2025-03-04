ea42a9184c47e6653a9863d20dea1b223633f809
regis066 <mugisharegis72@gmail.com>
2025-03-04 15:47:33 +0200

    feat: prevent client-side requests from being blocked by CORS policy

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
