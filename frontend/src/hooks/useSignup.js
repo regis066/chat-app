072c8d3987544a9aaaee3417caff5629ad55cf1c
regis066 <mugisharegis72@gmail.com>
2025-03-04 15:53:09 +0200

    feat: create useSignup hook for sending signup request to the server-side

---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
+
+const useSignup = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const signup = async ({
+    fullName,
+    username,
+    password,
+    confirmPassword,
+    gender,
+  }) => {
+    const success = handleInputErrors({
+      fullName,
+      username,
+      password,
+      confirmPassword,
+      gender,
+    });
+
+    if (!success) {
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const res = await fetch("http://localhost:5000/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          fullName,
+          username,
+          password,
+          confirmPassword,
+          gender,
+        }),
+      });
+
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      // add to localStorage
+      localStorage.setItem("chat-user", JSON.stringify(data));
+
+      // add to context
+      setAuthUser(data);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, signup };
+};
+
+export default useSignup;
+
+const handleInputErrors = ({
+  fullName,
+  username,
+  password,
+  confirmPassword,
+  gender,
+}) => {
+  // Validate input errors here
+  if (!fullName || !username || !password || !confirmPassword || !gender) {
+    toast.error("Please fill in all fields");
+    return false;
+  }
+  // Check password length
+  if (password.length < 6) {
+    toast.error("Password must be at least 6 characters");
+    return false;
+  }
+  // Check password match
+  if (password !== confirmPassword) {
+    toast.error("Passwords do not match");
+    return false;
+  }
+
+  return true;
+};
