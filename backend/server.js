57182cd7bfb76dcef3498380717d0edc9265ea64
regis066 <mugisharegis72@gmail.com>
2025-03-05 21:05:47 +0200

    refactor: remove cors middleware

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -14,7 +13,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
