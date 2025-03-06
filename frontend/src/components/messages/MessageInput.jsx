fd95642a2e788240ee9d3d8af523ddb9253983c3
regis066 <mugisharegis72@gmail.com>
2025-03-06 17:18:09 +0200

    feat: handle message input value and ensure it's sent to its destination

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,22 +1,44 @@
+import { useState } from "react";
 import { BsSend } from "react-icons/bs";
+import useSendMessage from "../../hooks/useSendMessage";
 const MessageInput = () => {
-	return (
-		<form className='px-4 my-3'>
-			<div className='w-full relative'>
-				<input
-					type='text'
-					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
-					placeholder='Send a message'
-				/>
-				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
-					<BsSend />
-				</button>
-			</div>
-		</form>
-	);
+  const [message, setMessage] = useState("");
+  const { loading, sendMessage } = useSendMessage();
+
+  const handleOnInputChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleOnSendMessage = async (e) => {
+    e.preventDefault();
+    await sendMessage(message);
+    setMessage("");
+  };
+  return (
+    <form className="px-4 my-3" onSubmit={handleOnSendMessage}>
+      <div className="w-full relative">
+        <input
+          type="text"
+          value={message}
+          onChange={handleOnInputChange}
+          className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
+          placeholder="Send a message"
+        />
+        <button
+          type="submit"
+          className="absolute inset-y-0 end-0 flex items-center pe-3"
+        >
+          {loading ? (
+            <span className="loading loading-spinner loading-lg"></span>
+          ) : (
+            <BsSend />
+          )}
+        </button>
+      </div>
+    </form>
+  );
 };
 export default MessageInput;
-
 
 // STARTER CODE SNIPPET
 // import { BsSend } from "react-icons/bs";
