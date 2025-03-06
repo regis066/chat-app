9e8876991b75ec0a7a759f4fb6549b3c55ee846a
regis066 <mugisharegis72@gmail.com>
2025-03-06 17:18:45 +0200

    refactor: provide dynamic interface

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,22 +1,25 @@
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
-	return (
-		<div className='px-4 flex-1 overflow-auto'>
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-		</div>
-	);
+  const { loading, messages } = useGetMessages();
+  console.log(messages);
+
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
+      {loading &&
+        [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start a conversation</p>
+      )}
+    </div>
+  );
 };
 export default Messages;
 
