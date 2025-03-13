fa40811fa8c8244e2fbeceb68a0bab3b8bea4479
regis066 <mugisharegis72@gmail.com>
2025-03-13 22:06:56 +0200

    feat: initialize socket io server and emit event to get online users

---
+++ backend/socket/socket.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const userSocketMap = {}; //{userId: socketId}
+io.on("connection", (socket) => {
+  console.log("🚀 A user connected:", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId != undefined) userSocketMap[userId] = socket.id;
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("❌ User disconnected:", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export { app, httpServer, io };
