6967e1cb6ce1ba4d98b249414c0f134d7f4f3396
regis066 <mugisharegis72@gmail.com>
2025-03-14 16:29:26 +0200

    refactor: update backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        // target: "http://localhost:5000",
+        target: "https://chat-app-server-kappa-one.vercel.app",
         changeOrigin: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
